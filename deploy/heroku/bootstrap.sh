09a3cd6265edd6f0737bf1e98215d6752ed5e840
Minh Hieu <hieu.dv@geekup.io>
2020-11-12 15:04:10 +0700

    Update java configuration to use cgroup memory limit in container

--- deploy/heroku/bootstrap.sh
+++ deploy/heroku/bootstrap.sh
@@ -23,21 +23,5 @@ envsubst "\$PORT" < /etc/nginx/conf.d/default.conf.template.1 > /etc/nginx/conf.
 
 nginx
 
-
-function get_maximum_heap(){ 
-    case $(ulimit -u) in
-        256)
-             maximum_heap=490
-            ;;
-        *)
-            maximum_heap=490
-            ;;
-    esac
-}
-
-get_maximum_heap
-
-application_run_command="java -Xmx${maximum_heap}m -Dserver.port=8080 -Djava.security.egd='file:/dev/./urandom' -jar server.jar"
-
-eval $application_run_command
+java -XX:+UseContainerSupport  -XX:MaxRAMPercentage=75.0 -Dserver.port=8080 -Djava.security.egd='file:/dev/./urandom' -jar server.jar
 
