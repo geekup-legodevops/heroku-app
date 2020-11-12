01bb1a51a7db34898e8eafc02fbd8223b8fc2505
geekup-legodevops <72587752+geekup-legodevops@users.noreply.github.com>
2020-11-12 15:18:42 +0700

    Update bootstrap.sh

--- deploy/heroku/bootstrap.sh
+++ deploy/heroku/bootstrap.sh
@@ -23,5 +23,5 @@ envsubst "\$PORT" < /etc/nginx/conf.d/default.conf.template.1 > /etc/nginx/conf.
 
 nginx
 
-java -XX:+UseContainerSupport -Dserver.port=8080 -Djava.security.egd='file:/dev/./urandom' -jar server.jar
+java -Xmx300m -Dserver.port=8080 -Djava.security.egd='file:/dev/./urandom' -jar server.jar
 
