f465476f326651f6846ada83f958bc9b268277e3
Minh Hieu <hieu.dv@geekup.io>
2020-11-12 11:06:19 +0700

    Update configuration

--- deploy/heroku/bootstrap.sh
+++ deploy/heroku/bootstrap.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
-application_port="$1"
 
-envsubst $1 < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+set -e
+
+if [[ -z "${APPSMITH_MAIL_ENABLED}" ]]; then
+    unset APPSMITH_MAIL_ENABLED
+fi
+
+if [[ -z "${APPSMITH_OAUTH2_GITHUB_CLIENT_ID}" ]] || [[ -z "${APPSMITH_OAUTH2_GITHUB_CLIENT_SECRET}" ]]; then
+    unset APPSMITH_OAUTH2_GITHUB_CLIENT_ID
+    unset APPSMITH_OAUTH2_GITHUB_CLIENT_SECRET
+fi
+
+if [[ -z "${APPSMITH_OAUTH2_GOOGLE_CLIENT_ID}" ]] || [[ -z "${APPSMITH_OAUTH2_GOOGLE_CLIENT_SECRET}" ]]; then
+    unset APPSMITH_OAUTH2_GOOGLE_CLIENT_ID
+    unset APPSMITH_OAUTH2_GOOGLE_CLIENT_SECRET
+fi
+
+
+cat /etc/nginx/conf.d/default.conf.template | envsubst "$(printf '$%s,' $(env | grep -Eo '^APPSMITH_[A-Z0-9_]+'))" | sed -e 's|\${\(APPSMITH_[A-Z0-9_]*\)}||g' > /etc/nginx/conf.d/default.conf.template.1
+
+envsubst "\$PORT" < /etc/nginx/conf.d/default.conf.template.1 > /etc/nginx/conf.d/default.conf
 
 nginx
+
 
 function get_maximum_heap(){ 
     case $(ulimit -u) in
         256)
-            maximum_heap=490
+             maximum_heap=490
             ;;
         *)
-            maximum_heap=900
+            maximum_heap=490
             ;;
     esac
 }
