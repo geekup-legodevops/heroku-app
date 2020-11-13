b29206bf944e9dcd19be68fd4e50f52fbdfd948c
Minh Hieu <hieu.dv@geekup.io>
2020-11-13 22:00:23 +0700

    Update bootstrap to check for github & google oauth env

--- deploy/heroku/bootstrap.sh
+++ deploy/heroku/bootstrap.sh
@@ -27,6 +27,16 @@ if [[ -z "${APPSMITH_MAIL_ENABLED}" ]]; then
     unset APPSMITH_MAIL_ENABLED # If this field is empty is might cause application crash
 fi
 
+if [[ -z "${APPSMITH_OAUTH2_GITHUB_CLIENT_ID}" ]] || [[ -z "${APPSMITH_OAUTH2_GITHUB_CLIENT_SECRET}" ]]; then
+    unset APPSMITH_OAUTH2_GITHUB_CLIENT_ID # If this field is empty is might cause application crash
+    unset APPSMITH_OAUTH2_GITHUB_CLIENT_SECRET
+fi
+
+if [[ -z "${APPSMITH_OAUTH2_GOOGLE_CLIENT_ID}" ]] || [[ -z "${APPSMITH_OAUTH2_GOOGLE_CLIENT_SECRET}" ]]; then
+    unset APPSMITH_OAUTH2_GOOGLE_CLIENT_ID # If this field is empty is might cause application crash
+    unset APPSMITH_OAUTH2_GOOGLE_CLIENT_SECRET
+fi
+
 cat /etc/nginx/conf.d/default.conf.template | envsubst "$(printf '$%s,' $(env | grep -Eo '^APPSMITH_[A-Z0-9_]+'))" | sed -e 's|\${\(APPSMITH_[A-Z0-9_]*\)}||g' > /etc/nginx/conf.d/default.conf.template.1
 
 envsubst "\$PORT" < /etc/nginx/conf.d/default.conf.template.1 > /etc/nginx/conf.d/default.conf
