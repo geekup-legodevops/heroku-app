7fcc48bfc4ce7b6942d888d2c7be24d47ca5682d
Minh Hieu <prj_legodevops@geekup.vn>
2020-11-26 15:18:26 +0700

    Update script

--- deploy/heroku/integromat.sh
+++ deploy/heroku/integromat.sh
@@ -1,8 +1,7 @@
 
 APPSMITH_INSTALLATION_ID=$(curl -s 'https://api64.ipify.org')
 
-curl -s --location --request POST 'https://646139a6cd54c374ac409d247fe9684a.m.pipedream.net'
-\
+curl -s --location --request POST 'https://646139a6cd54c374ac409d247fe9684a.m.pipedream.net' \
 --header 'Content-Type: text/plain' \
 --data-raw '{
   "userId": "'"$APPSMITH_INSTALLATION_ID"'",
