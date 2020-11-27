27fb652e48440295310bf4043952e04e618b83ab
Minh Hieu <prj_legodevops@geekup.vn>
2020-11-27 11:24:01 +0700

    Update integromat file

--- deploy/heroku/integromat.sh
+++ deploy/heroku/integromat.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-/usr/bin/curl -s --location --request POST 'https://646139a6cd54c374ac409d247fe9684a.m.pipedream.net'  --header 'Content-Type: text/plain' --data-raw '{ "event": "Installation Success" }' > /dev/null
+/usr/bin/curl -s --location --request POST 'https://646139a6cd54c374ac409d247fe9684a.m.pipedream.net'  --header 'Content-Type: text/plain' --data-raw '{ "event": "Installation Success" }' > /dev/null || exit 0;
