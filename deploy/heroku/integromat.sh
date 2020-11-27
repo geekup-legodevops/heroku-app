23ee59377b40a3a470d540ebe5612931d5730c2b
Minh Hieu <prj_legodevops@geekup.vn>
2020-11-27 09:21:40 +0700

    Update docker

--- deploy/heroku/integromat.sh
+++ deploy/heroku/integromat.sh
@@ -1,13 +1,14 @@
-echo "App run"
-# APPSMITH_INSTALLATION_ID=$(curl -s 'https://api64.ipify.org')
+#!/bin/sh
 
-# curl -s --location --request POST 'https://646139a6cd54c374ac409d247fe9684a.m.pipedream.net' \
-# --header 'Content-Type: text/plain' \
-# --data-raw '{
-#   "userId": "'"$APPSMITH_INSTALLATION_ID"'",
-#   "event": "Installation Success",
-#   "data": {
-#       "platform": "heroku",
-#       "os": "Ubuntu"
-#    }
-# }' > /dev/null
+APPSMITH_INSTALLATION_ID=$(curl -s 'https://api64.ipify.org')
+
+curl -s --location --request POST 'https://646139a6cd54c374ac409d247fe9684a.m.pipedream.net' \
+--header 'Content-Type: text/plain' \
+--data-raw '{
+  "userId": "'"$APPSMITH_INSTALLATION_ID"'",
+  "event": "Installation Success",
+  "data": {
+      "platform": "heroku",
+      "os": "Ubuntu"
+   }
+}' > /dev/null
