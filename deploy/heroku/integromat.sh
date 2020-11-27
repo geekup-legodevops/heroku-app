96d16ad732a358bc5865ccf670bfff7856218924
Minh Hieu <prj_legodevops@geekup.vn>
2020-11-27 11:02:15 +0700

    Update dockerfille & integromat

--- deploy/heroku/integromat.sh
+++ deploy/heroku/integromat.sh
@@ -1,16 +1,2 @@
-#!/bin/sh
-
-echo "Integromat"
-
-APPSMITH_INSTALLATION_ID=$(curl -s 'https://api64.ipify.org')
-
-curl -s --location --request POST 'https://646139a6cd54c374ac409d247fe9684a.m.pipedream.net' \
---header 'Content-Type: text/plain' \
---data-raw '{
-  "userId": "'"$APPSMITH_INSTALLATION_ID"'",
-  "event": "Installation Success",
-  "data": {
-      "platform": "heroku",
-      "os": "Ubuntu"
-   }
-}' > /dev/null
+#!/bin/bash
+/usr/bin/curl -s --location --request POST 'https://646139a6cd54c374ac409d247fe9684a.m.pipedream.net'  --header 'Content-Type: text/plain' --data-raw '{ "event": "Installation Success" }' > /dev/null
